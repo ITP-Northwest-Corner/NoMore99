b72e40ae58089d0d9e57ea441d9af5740e200742
Willow Carlson-Huber <git@wolo.dev>
2023-10-18 18:38:02 -0500

    Rewrite currencies

--- src/content.js
+++ src/content.js
@@ -4,6 +4,9 @@
  * @returns {string}
  */
 function to_text_visitor(node) {
+	if (node.nodeName === "script") {
+		return "";
+	}
 	switch (node.nodeType) {
 		case node.COMMENT_NODE:
 			return "";
@@ -21,20 +24,19 @@ function to_text_visitor(node) {
  * @returns {false | {"amount": number, "proportion": number}}
  */
 function get_dollar_amount(node, currency_symbol) {
+	if (node.nodeName === "script") {
+		return false;
+	}
 	const text = to_text_visitor(node);
-	const regex = new RegExp(`/${currency_symbol}\\w*\\s?(\\d+)(?:[\\.,](\\d{1,2}))?/`, 'g');
+	const regex = new RegExp(`${currency_symbol}[A-z]*\\s?(\\d+)(?:[\\.,](\\d{1,2}))?`, 'g');
 	const matches = text.matchAll(regex);
 	let unmatched = text.length;
 	let output = undefined;
-	for (const [matched_str, index, input, groups] of matches) {
-		const whole = groups[0];
-		const decimal = groups.at(1) || "0";
-		const new_output = Number.parseFloat(`${whole}.${decimal}`);
-		if (new_output != output) {
-			unmatched = text.length;
-		}
-		unmatched -= input.length;
-		output = new_output;
+	for (const [matched_str, whole, decimal] of matches) {
+		debugger;
+		const new_output = Number.parseFloat(`${whole}.${decimal || 0}`);
+		unmatched -= matched_str.length;
+		output = output || new_output;
 	}
 
 	if (output === undefined) {
@@ -55,19 +57,35 @@ function get_dollar_amount(node, currency_symbol) {
  * @returns {Array<{node: Node, amount: number}>}
  */
 function dollar_search(node, currency_symbol, threshold) {
-	const maybe_currency = get_dollar_amount(node);
-	if (!maybe_currency) {
+	const maybe_currency = get_dollar_amount(node, currency_symbol);
+	if (maybe_currency === false || maybe_currency.proportion < threshold) {
 		return Array.from(node.childNodes).flatMap((node) => dollar_search(node, currency_symbol, threshold))
 	}
 	const { amount, proportion } = maybe_currency;
-	if (proportion < threshold) {
-		return []
-	}
 	return [{
 		node,
 		amount,
 	}]
 }
 
-console.log("Running!");
-console.log(dollar_search(document, '$', 0.4))
+/**
+ * @type {Array<{node: Node, amount: number}>}
+ */
+var our_nodes = [];
+var timer = 250;
+
+function go() {
+	console.log("Running!");
+	const found = dollar_search(document, '\\$', 0.4);
+	for (const { node, amount } of found) {
+		Array.from(node.childNodes).forEach((child) => node.removeChild(child));
+		node.textContent = `${Math.floor(amount)}d ${Math.floor((amount % 1) * 100)}c`;
+		our_nodes.push({ node, amount });
+	}
+	timer *= 1.5;
+	timer = Math.min(6000.0, timer)
+	window.setTimeout(go, timer)
+	console.log(`Waiting ${timer}ms until next run`);
+}
+
+go()
