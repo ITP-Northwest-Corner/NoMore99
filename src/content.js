f3a739a7c90254bafff31ae13a8f3c473807adfb
Willow Carlson-Huber <git@wolo.dev>
2023-11-01 19:25:14 -0500

    some additions

--- src/content.js
+++ src/content.js
@@ -83,19 +83,80 @@ function findDollarAmounts(node, currencySymbol, threshold) {
 	}];
 }
 
+/***
+ * @type {Object.<string, number>}
+ */
+const units = {DOLLARS: 1, ...browser.storage.local.get('units').units};
+
+function convertBetween(quantity, from, to) {
+	return (quantity * from) / to;
+}
+
+/***
+ * @type {Object.<string, {applicable: (number) => boolean, convert: (number) => string}}
+ */
+const conversions = {
+	pickAUnit: {
+		applicable(_price) {
+			return Object.keys(units)
+				.some(name => !['DOLLARS', 'hours'].includes(name));
+		},
+		get convert() {
+			const strangeUnits = Object.keys(units)
+				.filter(name => !['DOLLARS', 'hours'].includes(name));
+			const unit = strangeUnits[Math.floor(Math.random() * strangeUnits.length)];
+			return price => {
+				const converted = convertBetween(price, units.DOLLARS, units[unit]);
+				if (Math.abs(converted % 1) < 0.2) {
+					return `${Math.round(price)} ${unit}`;
+				}
+
+				return `${price} ${unit}`;
+			};
+		},
+	},
+	fallback: {
+		applicable: _ => false,
+		convert(price) {
+			const cost = convertBetween(price, units.DOLLARS, units.hours || 20);
+			const hours = Math.floor(cost);
+			const minutes = Math.floor((cost * 60) - (hours * 60));
+			if (Math.abs(convertBetween(price, units.DOLLARS, units.hours || 20) % 1) < 0.2) {
+				return `${Math.round(hours)}`;
+			}
+
+			return `${hours}:${minutes}`;
+		},
+	},
+};
+
+function pickConvert(price) {
+	const functions = Object
+		.keys(conversions)
+		.filter(name => conversions[name].applicable(price))
+		.map(name => conversions[name].convert);
+
+	if (functions.length === 0) {
+		return conversions.fallback;
+	}
+
+	return functions[Math.floor(Math.random() * functions.length())];
+}
+
 /**
- * @type {Array<{node: Node, amount: number}>}
+ * @type {Array<{node: Node, amount: number, convert: (number) => string}>}
  */
 const ourNodes = [];
 let timer = 250;
 
 async function go(doTimer = true) {
 	console.log('Running!');
+	Object.assign(units, browser.storage.local.get('units').units);
 	const found = findDollarAmounts(document, '\\$', 0.4);
 	for (const {node, amount} of found) {
-		const existing = ourNodes.some(({node: otherNode, amount: _}) => node === otherNode);
+		const existing = ourNodes.some(({node: otherNode, amount: _, convert: __}) => node === otherNode);
 		if (!existing) {
-			ourNodes.push({node, amount});
+			ourNodes.push({node, amount, convert: pickConvert(amount)});
 		}
 
 		for (const child of Array.from(node.childNodes)) {
@@ -105,9 +166,9 @@ async function go(doTimer = true) {
 
 	const {doReplace} = await browser.storage.local.get('doReplace');
 	console.log(doReplace);
-	for (const {node, amount} of ourNodes) {
+	for (const {node, amount, convert} of ourNodes) {
 		node.textContent = doReplace
-			? `${Math.floor(amount)}d ${Math.floor((amount % 1) * 100)}c`
+			? convert(amount)
 			: `$${Math.floor(amount)}.${Math.floor((amount % 1) * 100)}`;
 	}
 
--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-await-in-loop */
 const qs = s => document.querySelector(s);
-const g = k => browser.storage.local.get(k);
+const g = k => browser.storage.local.get(k)[k];
 // Const s = (k, v) => browser.storage.local.set(k, v);
 
 qs('#rerun').addEventListener('click', async _event => {
@@ -18,10 +19,16 @@ pullFromStorage();
 
 async function pullFromStorage() {
 	qs('#do_replace').checked = await g('doReplace');
+	for (const element of document.querySelector('.unit')) {
+		const amt = await g('units');
+	}
 }
 
 function pushToStorage() {
 	browser.storage.local.set({
 		doReplace: qs('#do_replace').checked,
 	});
+	for (const element of document.querySelector('.unit')) {
+		const amt = element.nodeValue;
+	}
 }
